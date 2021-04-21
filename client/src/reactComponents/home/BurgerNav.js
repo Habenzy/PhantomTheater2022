6de2447a91737c39c57f57adaabca8c621020dad
ohartwig <45525308+otwig@users.noreply.github.com>
2021-04-21 16:53:44 -0400

    merged burgerFixes

--- client/src/reactComponents/home/BurgerNav.js
+++ client/src/reactComponents/home/BurgerNav.js
@@ -8,52 +8,6 @@ import Menu from 'react-burger-menu/lib/menus/slide'
 
 
 // Nav Bar functionality with links to home, about , artists, season and reserve components
-// class BurgerNav extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {menuOpen:false}
-//     this.handleClick = this.handleClick.bind(this)
-//   }
-//   handleClick() {
-//     console.log('click')
-//   }
-
-//   handleStateChange(state) {
-//     this.setState({menuOpen:state.isOpen})
-//     console.log('handStateChange')
-//   }
-
-//   closeMenu () {
-//     this.setState({menuOpen:false})
-//     console.log('closeMenu')
-//   }
-
-//   toggleMenu () {
-//     this.setState(state => ({menuOpen: !state.menuOpen}))
-//   }
-
-//   render() {
-//     return (
-//       <div className="burgerNavBar">
-//         {/*menu */}
-//         <Menu
-//         className='menu'
-//         isOpen={this.state.menuOpen} 
-//         onClick={(state) => this.handleStateChange(state)}
-//       >
-//         <li onClick={() => this.closeMenu()}><Link to="/">Home</Link></li>
-//         <li onClick={() => this.closeMenu()}><Link to="/About">About</Link></li>
-//         <li onClick={() => this.closeMenu()}><Link to="/AllArtist">Featured</Link></li>
-//         <li onClick={() => this.closeMenu()}><Link to="/Season">Season</Link></li>
-//         <li onClick={() => this.closeMenu()}><Link to="/AllArtist">Artists</Link></li>
-
-
-//         </Menu>
-//       </div >
-
-//     )
-//   }
-// }
 
 const BurgerNav = (props) => {
  
