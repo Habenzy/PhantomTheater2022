2df22c596e82304a3af942d8711b39fe2012bbc3
Ben Chussid <bschussid@aol.com>
2021-04-21 16:30:25 -0400

    unreturn

--- client/src/reactComponents/home/Artist.js
+++ client/src/reactComponents/home/Artist.js
@@ -1,14 +1,10 @@
 // ---------------------Imports --------------------------
 import React, { useState } from "react";
-import ArtistEvent from "../forms/ArtistEvent"
+import ArtistEvent from "../forms/ArtistEvent";
 import { firestore } from "../firebase/firebase";
-import './Artist.css'
-
-
+import "./Artist.css";
 
 export default function Artist() {
-
-
   let [artist, setArtist] = useState("");
   let [bio, setBio] = useState("");
   let [image1, setImage1] = useState("");
@@ -20,8 +16,6 @@ export default function Artist() {
   let [artistYouTube, setArtistYouTube] = useState("");
   let [artistInstagram, setArtistInstagram] = useState("");
   let [artistSpotify, setArtistSpotify] = useState("");
-
-
 
   async function getOneShow() {
     let id = document.location.hash.substring(1);
@@ -39,38 +33,42 @@ export default function Artist() {
       showInData.image2 ? setImage2(showInData.image2) : setImage2("");
       showInData.image3 ? setImage3(showInData.image3) : setImage3("");
 
-
-      showInData.artistWebsite ? setArtistWebsite(showInData.artistWebsite) : setArtistWebsite("");
-      showInData.artistFacebook ? setArtistFacebook(showInData.artistFacebook) : setArtistFacebook("");
-      showInData.artistYouTube ? setArtistYouTube(showInData.artistYouTube) : setArtistYouTube("");
-      showInData.artistInstagram ? setArtistInstagram(showInData.artistInstagram) : setArtistInstagram("");
-      showInData.artistSpotify ? setArtistSpotify(showInData.artistSpotify) : setArtistSpotify("");
+      showInData.artistWebsite
+        ? setArtistWebsite(showInData.artistWebsite)
+        : setArtistWebsite("");
+      showInData.artistFacebook
+        ? setArtistFacebook(showInData.artistFacebook)
+        : setArtistFacebook("");
+      showInData.artistYouTube
+        ? setArtistYouTube(showInData.artistYouTube)
+        : setArtistYouTube("");
+      showInData.artistInstagram
+        ? setArtistInstagram(showInData.artistInstagram)
+        : setArtistInstagram("");
+      showInData.artistSpotify
+        ? setArtistSpotify(showInData.artistSpotify)
+        : setArtistSpotify("");
     }
-
   }
-  getOneShow()
-
+  getOneShow();
 
   return (
     <div className="artistsContainer">
       <h1>Artists</h1>
-    
-        return <ArtistEvent
-          artist={artist}
-          image1={image1}
-          image2={image2}
-          image3={image3}
 
-          bio={bio}
-          email={email}
-          website={artistWebsite}
-          facebook={artistFacebook}
-          youtube={artistYouTube}
-          instagram={artistInstagram}
-          spotify={artistSpotify}
-        ></ArtistEvent >
-      
-    </div >
-
-  )
+      <ArtistEvent
+        artist={artist}
+        image1={image1}
+        image2={image2}
+        image3={image3}
+        bio={bio}
+        email={email}
+        website={artistWebsite}
+        facebook={artistFacebook}
+        youtube={artistYouTube}
+        instagram={artistInstagram}
+        spotify={artistSpotify}
+      ></ArtistEvent>
+    </div>
+  );
 }
