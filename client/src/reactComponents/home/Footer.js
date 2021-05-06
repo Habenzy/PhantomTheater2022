1cc7d719326ebd9ce850074d101846c287d52c75
Ben Chussid <bschussid@aol.com>
2021-05-06 16:27:33 -0400

    link to admin login page added to homepage

--- client/src/reactComponents/home/Footer.js
+++ client/src/reactComponents/home/Footer.js
@@ -1,9 +1,9 @@
 //---------------------------------------------Imports---------------------------------------------------
-import React from 'react'
-import './Footer.css'
-import Facebook from '../images/facebook.png'
-import Instagram from '../images/instagram.png'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Footer.css";
+import Facebook from "../images/facebook.png";
+import Instagram from "../images/instagram.png";
+import { Link } from "react-router-dom";
 
 //------------ Footer function returning Footer containing Social Media Links and Copyright Info ---------
 
@@ -11,17 +11,27 @@ function Footer() {
   return (
     <div className="footer">
       <div className="social_images">
-        <a href="https://www.facebook.com/Phantom-Theater-1730271753886842/?ref=page_internal" target="blank"><img className='social' src={Facebook} alt=''/></a>
-        <a href="https://www.instagram.com/phantomtheater/" target="blank"><img className='social' id="insta" src={Instagram} alt=''/></a>
+        <a
+          href="https://www.facebook.com/Phantom-Theater-1730271753886842/?ref=page_internal"
+          target="blank"
+        >
+          <img className="social" src={Facebook} alt="" />
+        </a>
+        <a href="https://www.instagram.com/phantomtheater/" target="blank">
+          <img className="social" id="insta" src={Instagram} alt="" />
+        </a>
       </div>
-        <div className="footerLinks"><Link to='/ProposalForm'>Submit a Show Proposal</Link></div>
-        
+      <div className="footerLinks">
+        <Link to="/ProposalForm">Submit a Show Proposal</Link>
+      </div>
+
       <div>
         <p>Copyright Phantom Theater 2020</p>
       </div>
+      <Link to="/login">Admin Login</Link>
     </div>
-  )
+  );
 }
 
 //------export the component---------
-export default Footer
+export default Footer;
