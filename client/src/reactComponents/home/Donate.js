deecf503e3dbfca59adbc2044387ce1b45de3085
ohartwig <45525308+otwig@users.noreply.github.com>
2021-05-12 17:14:22 -0400

    donate page created, copy added.

---
+++ client/src/reactComponents/home/Donate.js
@@ -0,0 +1,132 @@
+//-------------------------------------Imports--------------------------
+import React from "react";
+import "./Donate.css";
+
+function Donate() {
+  return (
+    <div className="donate">
+      <div className="donateWrapper">
+        <div className="copy">
+          <p>Phantom Theater is for and by artists and the community.  We are committed to supporting both through the performing arts.  While a portion of admissions to our shows helps offset our operating costs - including rent, utilities, and an honorarium for our artistic director - we rely on donations and grants for both seasonal costs and major upgrades.  For instance, in 2021 we replaced our archaic and faulty lighting system with a modern LED grid and board through the generosity of our patrons, a benefit performance by Wesli, and the Vermont Humanities Council.
+
+          Every donation counts!  And as a 501.c3 organization, every donation is also tax deductible.  Please consider adding your name to the list of donors today!
+</p>
+        </div>
+        <div className="sponsorsWrapper">
+          <h1>Sponsors</h1>
+          <p className="thankYou">PHANTOM THEATER IS PROUD TO COUNT MANY LOCAL BUSINESSES AND INDIVIDUALS AS FRIENDS
+          AND SUPPORTERS.
+            THANK YOU TO ALL OUR SPONSORS!</p>
+          <br />
+          <div className="sponsorsList">
+            About Being
+          <br />
+          American Flatbread
+          <br />
+          Artisans Hand
+          <br />
+          Baked Beads
+          <br />
+          John Barkhausen
+          <br />
+          Dana Barrows
+          <br />
+          Better Travel
+          <br />
+          Big Town Gallery
+          <br />
+          Broadleaf Landscape Architecture
+          <br />
+          Bliss Ridge
+          <br />
+          Bundy Modern
+          <br />
+          Butternut Systems
+          <br />
+          Chooseco
+          <br />
+          Complexions
+          <br />
+          Darrad Services
+          <br />
+          The Design Group
+          <br />
+          Elevation Physical Therapy
+          <br />
+          East Warren Community Market
+          <br />
+          Green Toolbox
+          <br />
+          High Country Hair
+          <br />
+          Trish Hopkins
+          <br />
+          Michael Levengood
+          <br />
+          Maclay Architects
+          <br />
+          Mad River Massage
+          <br />
+          Mad River Web
+          <br />
+          Mad River Hydro
+          <br />
+          Mad River Massage
+          <br />
+          Medusa Studio
+          <br />
+          Mehurons
+          <br />
+          Northern Reliability
+          <br />
+          The Pitcher Inn
+          <br />
+          R. H. Travers Company
+          <br />
+          Sanford Strauss Architects
+          <br />
+          Scout's Honor Creamery
+          <br />
+          Sellers & Company Architecture
+          <br />
+          Sugarhouse Soundworks
+          <br />
+          MacKenzie & Joel Taplin
+          <br />
+          The Tree House Guys
+          <br />
+          Tonewood Maple
+          <br />
+          Verilux
+          <br />
+          Vees Flowers
+          <br />
+          The Warren Store
+          <br />
+            <br />
+          </div>
+          <div className="line"></div>
+          <div className="sponsorHowTo">
+            <div>
+              Our sponsors help keep ticket prices low, make special events
+              possible, and spread the word about the varied events we present
+              each summer.
+            <br />
+              <br />
+            To become a sponsor or make a donation, please email us at
+            tracy@madriver.com or call (802) 496-5997.
+            <br />
+              <br />
+              <p className="special">SPECIAL THANKS TO THE WARREN ARTS COMMITTEE AND THE ZOOT FUND OF
+              THE VERMONT COMMUNITY FOUNDATION FOR HELPING US PURCHASE ALL NEW
+            FLOORING FOR OUR STAGE IN 2013!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+//------export the component---------
+export default Donate;
