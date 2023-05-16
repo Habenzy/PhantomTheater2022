0de81435ca5cd82f0c2581524b02fde3c9d87e32
habenzy <rstauss@burlingtoncodeacademy.com>
2023-05-16 10:24:20 -0400

    repositioned

--- client/src/reactComponents/forms/SeasonEvent.js
+++ client/src/reactComponents/forms/SeasonEvent.js
@@ -5,51 +5,48 @@ import "../formcss/seasonEvent.css";
 // -------- Season event component functionality and export
 
 function SeasonEvent(props) {
-  let showImage = (
-    <img src={props.imageLg} alt="" />
-  );
+  let showImage = <img src={props.imageLg} alt="" />;
   let numberOfShows = props.dates.length;
 
-
-   function changeDate(date) {
-      let months = {
-         "01": 'January',
-         "02": 'February',
-         "03": 'March',
-         "04": 'April',
-         "05": 'May',
-         "06": 'June',
-         "07": 'July',
-         "08": 'August',
-         "09": 'September',
-         "10": 'October',
-         "11": 'November',
-         "12": 'December'
-      }
-      //------- changing the military time string to normal time
-      let newTime;
-      ///sepparating the string at the T.
-      let dateFix = date.split("T");
-      let startDate = dateFix[0];
-      let month = startDate.split("-")[1];
-      let day = startDate.split("-")[2];
-      let endDate = months[month] + " " + day + '  -  ';
-      /// targetting second item of the dateFix array (which is the time)
-      let time = dateFix[1];
-      // hours is the first index item of the time array which was split at the :
-      let hours = time.split(":")[0];
-      let minutes = time.split(":")[1];
-      // if hours are a higher number than 12 (milt time)  
-      // then subtract 12 to convert to normal time
-      if (hours > 12) {
-         newTime = hours - 12 + ":" + minutes + " PM";
-      } else {
-         // otherwise the time will just print the number if it is lower than 12
-         newTime = hours + ":" + minutes + " AM";
-      }
-      let finalDate = endDate + " " + newTime;
-      return finalDate;
-   }
+  function changeDate(date) {
+    let months = {
+      "01": "January",
+      "02": "February",
+      "03": "March",
+      "04": "April",
+      "05": "May",
+      "06": "June",
+      "07": "July",
+      "08": "August",
+      "09": "September",
+      10: "October",
+      11: "November",
+      12: "December",
+    };
+    //------- changing the military time string to normal time
+    let newTime;
+    ///sepparating the string at the T.
+    let dateFix = date.split("T");
+    let startDate = dateFix[0];
+    let month = startDate.split("-")[1];
+    let day = startDate.split("-")[2];
+    let endDate = months[month] + " " + day + "  -  ";
+    /// targetting second item of the dateFix array (which is the time)
+    let time = dateFix[1];
+    // hours is the first index item of the time array which was split at the :
+    let hours = time.split(":")[0];
+    let minutes = time.split(":")[1];
+    // if hours are a higher number than 12 (milt time)
+    // then subtract 12 to convert to normal time
+    if (hours > 12) {
+      newTime = hours - 12 + ":" + minutes + " PM";
+    } else {
+      // otherwise the time will just print the number if it is lower than 12
+      newTime = hours + ":" + minutes + " AM";
+    }
+    let finalDate = endDate + " " + newTime;
+    return finalDate;
+  }
 
   return (
     <div className="season_container">
@@ -57,21 +54,51 @@ function SeasonEvent(props) {
       <br />
       {showImage}
       <br />
-      <p className="artist">
-      {props.artist}
-      </p>
+      <p className="artist">{props.artist}</p>
       <p className="blurb">{props.blurb}</p>
       <br />
-      
-      {numberOfShows >= 1 ? <p className="date">{changeDate(props.dates[0])}</p> : console.log()}
-      {numberOfShows >= 2 ? <p className="date">{changeDate(props.dates[1])}</p> : console.log()}
-      {numberOfShows >= 3 ? <p className="date">{changeDate(props.dates[2])}</p> : console.log()}
-      {numberOfShows >= 4 ? <p className="date">{changeDate(props.dates[3])}</p> : console.log()}
-      {numberOfShows >= 5 ? <p className="date">{changeDate(props.dates[4])}</p> : console.log()}
-      {numberOfShows >= 6 ? <p className="date">{changeDate(props.dates[5])}</p> : console.log()}
 
-       <div className="line"></div>
-       <a href="https://sevendaystickets.com/organizations/phantom-theater" target="_blank" className="buy-ticket">Buy Tickets</a>
+      {numberOfShows >= 1 ? (
+        <p className="date">{changeDate(props.dates[0])}</p>
+      ) : (
+        console.log()
+      )}
+      {numberOfShows >= 2 ? (
+        <p className="date">{changeDate(props.dates[1])}</p>
+      ) : (
+        console.log()
+      )}
+      {numberOfShows >= 3 ? (
+        <p className="date">{changeDate(props.dates[2])}</p>
+      ) : (
+        console.log()
+      )}
+      {numberOfShows >= 4 ? (
+        <p className="date">{changeDate(props.dates[3])}</p>
+      ) : (
+        console.log()
+      )}
+      {numberOfShows >= 5 ? (
+        <p className="date">{changeDate(props.dates[4])}</p>
+      ) : (
+        console.log()
+      )}
+      {numberOfShows >= 6 ? (
+        <p className="date">{changeDate(props.dates[5])}</p>
+      ) : (
+        console.log()
+      )}
+
+      <br></br>
+      <a
+        href="https://sevendaystickets.com/organizations/phantom-theater"
+        target="_blank"
+        className="buy-ticket"
+      >
+        Buy Tickets
+      </a>
+
+      <div className="line"></div>
     </div>
   );
 }
