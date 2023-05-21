e2df7b1e89d4715945dd73583e042c974e7df19c
habenzy <rstauss@burlingtoncodeacademy.com>
2023-05-21 10:11:37 -0400

    palceholder message while no shows

--- client/src/reactComponents/home/Season.js
+++ client/src/reactComponents/home/Season.js
@@ -36,7 +36,7 @@ function Season() {
     
     <div className="season_container">
       <h1>Season 2023</h1>
-      { allShows ?
+      { allShows.length ?
         
         allShows.map(show => {
         return <SeasonEvent
@@ -51,7 +51,7 @@ function Season() {
 
         ></SeasonEvent>
 
-      }) : 'loading'
+      }) : <p>The 2023 season is under construction... Shows and show times will be posted soon</p>
 
       }
 
