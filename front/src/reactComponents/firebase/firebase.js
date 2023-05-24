f209c5dbf9be3b32adc21a4e998e4cca49a5fc09
habenzy <rstauss@burlingtoncodeacademy.com>
2023-05-24 10:18:33 -0400

    firebase issues resolved (mostly)

--- front/src/reactComponents/firebase/firebase.js
+++ front/src/reactComponents/firebase/firebase.js
@@ -1,8 +1,8 @@
 //-----------------------Imports -----------------
 import { initializeApp } from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
-import "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const app = initializeApp({
   apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
@@ -16,9 +16,9 @@ const app = initializeApp({
 
 
 //-----------export the database------
-export const firestore = app.firestore();
-export const auth = app.auth();
-export const storage = app.storage();
+export const firestore = getFirestore(app);
+export const auth = getAuth();
+export const storage = getStorage(app);
 
 //------- use firebase internal timestamps-------
 firestore.settings({ timestampsInSnapshots: true });
